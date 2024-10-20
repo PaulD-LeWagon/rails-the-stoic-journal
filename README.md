8b9e707818002cc5e974222995cf2177cfcb24b2
Paul Devanney <pauldevanney92@gmail.com>
2024-10-20 01:32:41 +0100

    experimenting with dragular and creating an interface

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 Rails app generated with [lewagon/rails-templates](https://github.com/lewagon/rails-templates), created by the [Le Wagon coding bootcamp](https://www.lewagon.com) team.
+
+
+<div id="<%= dom_id task %>_sub_tasks" class="<%= dom_id task %>_sub_tasks collapse-container collapse" data-controller="dragula"></div>
+
+          <% task.sub_tasks.each do |subtask| %>
+
+            <div class="card subtask m-1">
+              <div class="card-header d-flex justify-content-between align-items-center">
+                <h3><i class="fa-solid fa-grip handle"></i> #<span><%= subtask.order %></span>  <%= subtask.title %></h3>
+                <div class="d-flex justify-content-end">
+                  <%= render 'sub_tasks/links', sub_task: subtask %>
+                  <!--
+                  <i class="fa-solid fa-square-check"></i>
+                  <i class="fa-regular fa-square-check"></i>
+                  -->
+                  <a href="#" type="button"
+                    class="collapser btn btn-warning m-1"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#<%= dom_id subtask %>"
+                    aria-expanded="false"
+                    aria-controls="<%= dom_id subtask %>">
+                    <i class="fa-solid fa-chevron-down"></i>
+                  </a>
+                </div>
+              </div>
+              <div class="card-body collapse" id="<%= dom_id subtask %>">
+                <p class="card-text bd-font"><%= subtask.description %></p>
+              </div>
+            </div>
+
+          <% end %>
+
+        </div>
--- app/javascript/controllers/dragula_controller.js
+++ app/javascript/controllers/dragula_controller.js
@@ -1,36 +1,68 @@
 import { Controller } from "@hotwired/stimulus"
 
+let counter = 0;
+
 const log = something => console.log(typeof something, something)
 
-const setCardOrdinals = (container) => {
-  const cards = container.querySelectorAll('.card.subtask');
-  cards.forEach((card, i) => {
+const setCardOrdinals = (cards) => {
+  Array.from(cards).forEach((card, i) => {
     const j = i + 1;
     card.querySelectorAll('.card-header span')[0].innerText = j;
   });
 }
-
 // Connects to data-controller="dragula"
 export default class extends Controller {
-  connect() {
+  initialize() {
 
-    setCardOrdinals(this.element);
+    const $collConts = $('.collapse-container');
+    $collConts.on('shown.bs.collapse', (e) => {
+      const $btn = $(`[aria-controls=${e.target.id}]`);
+      const icon = $btn.find('i').get(0);
+      if(icon.classList.contains('fa-chevron-down')) {
+        icon.classList.remove('fa-chevron-down')
+        icon.classList.add('fa-chevron-up')
+      } else if(icon.classList.contains('fa-angles-down')) {
+        icon.classList.remove('fa-angles-down')
+        icon.classList.add('fa-angles-up')
+      }
+    })
+    .on('hidden.bs.collapse', (e) => {
+      const $btn = $(`[aria-controls=${e.target.id}]`);
+      const icon = $btn.find('i').get(0);
+      if(icon.classList.contains('fa-chevron-up')) {
+        icon.classList.remove('fa-chevron-up')
+        icon.classList.add('fa-chevron-down')
+      } else if(icon.classList.contains('fa-angles-up')) {
+        icon.classList.remove('fa-angles-up')
+        icon.classList.add('fa-angles-down')
+      }
+    });
 
-    const containers = [];
+    let reap = this.element.querySelectorAll(':scope > .task');
+    if (reap.length) {
+      setCardOrdinals(reap);
+    } else {
+      reap = this.element.querySelectorAll('.subtask');
+      setCardOrdinals(reap);
+    }
 
-    containers.push(this.element);
-
-    console.log(containers);
-
-    const drake = dragula(containers, {
-      // moves: function (el, container, handle) {
-      //   console.log(handle.classList.contains('handle'))
-      //   return handle.classList.contains('handle');
-      // }
+    const drake = dragula([this.element], {
+      // moves: (el, container, handle) => handle.classList.contains('handle'),
+      revertOnSpill: true,
+      accepts: (el, target, source, sibling) => {
+        log(sibling)
+        return false;
+      }
     });
     drake.on('drop', (el, target) => {
-      setCardOrdinals(this.element);
+      setCardOrdinals(el.parentNode.children);
     });
 
+  }
+
+  connect() {
+    // counter += 1;
+    // log('Connecting...')
+    // log(counter)
   }
 }
